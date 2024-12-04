a975047630751dcc317f6eb5861f82806d1639d0
Yanik <emmenegger@yanik.pro>
2024-12-04 10:28:41 +0100

    combine chatbot and review tool

--- next-js-app/src/app/api/auth/confirm/route.tsx
+++ next-js-app/src/app/api/auth/confirm/route.tsx
@@ -43,5 +43,6 @@ export async function GET(request: NextRequest) {
 
     // return the user to an error page with some instructions
     redirectTo.pathname = '/error'
-    return NextResponse.redirect(redirectTo)
+    //return NextResponse.redirect(redirectTo)
+    return redirectTo
 }
