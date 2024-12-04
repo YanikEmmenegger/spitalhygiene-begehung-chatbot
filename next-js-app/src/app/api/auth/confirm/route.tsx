d932a0c19a36eeb8722e1b7ee74c07ee6345f948
Yanik <emmenegger@yanik.pro>
2024-12-04 11:03:51 +0100

    troubleshooting api

--- next-js-app/src/app/api/auth/confirm/route.tsx
+++ next-js-app/src/app/api/auth/confirm/route.tsx
@@ -10,29 +10,38 @@ export async function GET(request: NextRequest) {
     const {searchParams} = new URL(request.url)
     const token_hash = searchParams.get('token_hash')
     const type = searchParams.get('type') as EmailOtpType | null
+    const next = '/'
+
+    // Create redirect link without the secret token
+    const redirectTo = request.nextUrl.clone()
+    redirectTo.pathname = next
+    redirectTo.searchParams.delete('token_hash')
+    redirectTo.searchParams.delete('type')
+
+    //set cookie for 30 days called userAuthenticated
+    const cookieStore = await cookies()
+    cookieStore.set({
+        name: 'userAuthenticated',
+        value: 'true',
+        httpOnly: true,
+        path: '/',
+    })
+
 
     if (token_hash && type) {
         const supabase = await createClient()
 
-        const {error, data} = await supabase.auth.verifyOtp({
+        const {error} = await supabase.auth.verifyOtp({
             type,
             token_hash,
         })
-        //set cookie for 30 days called userAuthenticated
-        const cookieStore = await cookies()
-        cookieStore.set({
-            name: 'userAuthenticated',
-            value: data.session?.access_token || 'true',
-            httpOnly: true,
-            path: '/',
-            secure: true,
-        })
-
         if (!error) {
-
-            return NextResponse.redirect(process.env.NEXT_PUBLIC_URL!)
+            redirectTo.searchParams.delete('next')
+            return NextResponse.redirect(redirectTo)
         }
     }
 
-    return NextResponse.redirect(process.env.NEXT_PUBLIC_URL!)
+    // return the user to an error page with some instructions
+    redirectTo.pathname = '/error'
+    return NextResponse.redirect(redirectTo)
 }
